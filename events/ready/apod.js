bb9b6449c719f85f0bce69ea3035325d28f70524
Brandt Hughes <brandt@brandthughes.com>
2023-06-20 18:13:01 -0500

    initial commit
    should create an embed in a designated channel once per day filled with NASA's astronomy photo of the day

---
+++ events/ready/apod.js
@@ -0,0 +1,88 @@
+require('dotenv').config();
+const { EmbedBuilder } = require('discord.js');
+const { request } = require('undici');
+
+module.exports = async (client) => {
+	let testChannel = client.channels.cache.find(channel => channel.id === process.env.APOD_CHANNEL);
+	let day = 1000 * 60 * 60 * 24;
+
+	setInterval(async () => {
+		//testChannel.send('testing an interval message every 5 seconds');
+		console.log('interval function begins now');
+
+		let apodResponse = await request(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API}`);
+		let data = await apodResponse.body.json();
+
+		console.log(`data requested and retrieved`);
+
+		// code adapted from Raxlitude's Discord Daily NASA APOD Posts on Autocode
+		// checks if `copyright` field is present in the JSON data - if, then includes `footer` field of embed to display copyright holder(s)
+		if ('copyright' in data) {
+			// checks if the post type is image & copyrighted
+			if (data.media_type === 'image') {
+				let embed = new EmbedBuilder()
+					.setTitle(data.title)
+					.setURL(data.url)
+					.setDescription(data.explanation)
+					.setImage(`${data.url}`)
+					.setColor(0x0165b3)
+					.setTimestamp(`${data.date}`)
+					.setFooter({ text: `Copyright - ${data.copyright}` }); // please keep this due to NASA's restrictions
+				
+				await testChannel.send({ embeds: [embed] });
+				console.log('embed sent with image and copyright');
+
+			// sending a different message if the post is not an image & copyrighted
+			} else {
+				let embed = new EmbedBuilder()
+					.setTitle(data.title)
+					.setURL(data.url)
+					.setDescription(data.explanation)
+					.setImage(`${data.thumbnail}`)
+					.setColor(0x0165b3)
+					.addFields({
+						name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
+						value: data.url,
+						inline: false,
+					})
+					.setTimestamp(`${data.date}`)
+					.setFooter({ text: `Copyright - ${data.copyright}` }); // please keep this due to NASA's restrictions
+									
+				await testChannel.send({ embeds: [embed] });
+				console.log('embed sent w/o image but with copyright');
+			}
+
+		// checks if the post type is a video and not copyrighted
+		} else if (data.media_type === 'video') {
+			let embed = new EmbedBuilder()
+				.setTitle(data.title)
+				.setURL(data.url)
+				.setDescription(data.explanation)
+				.setImage(`${data.thumbnail}`)
+				.setColor(0x0165b3)
+				.addFields({
+					name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
+					value: data.url,
+					inline: false,
+				})
+				.setTimestamp(`${data.date}`);
+		
+			await testChannel.send({ embeds: [embed] });
+			console.log('embed sent with video placeholder but no copyright');
+			
+		// checks if the post type is an image and not copyrighted
+		} else if (data.media_type === 'image') {
+			let embed = new EmbedBuilder()
+				.setTitle(data.title)
+				.setURL(data.url)
+				.setDescription(data.explanation)
+				.setImage(`${data.url}`)
+				.setColor(0x0165b3)
+				.setTimestamp(`${data.date}`);
+		
+			await testChannel.send({ embeds: [embed] });
+			console.log('embed sent with image but no copyright');
+		}
+	}, day
+	);
+};
