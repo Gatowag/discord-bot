9029bd0a2a93ebd60eae277f2091fa7924704d66
Brandt Hughes <brandt@brandthughes.com>
2023-06-20 22:21:20 -0500

    initial commit
    accepts links from youtube shorts and returns a standard youtube link which places the short into the regular youtube embed and interface, optional lengthy return

---
+++ commands/utilities/yt-shorts-unfurl.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('yt-shorts-unfurl')
+		.setDescription('Convert a youtube shorts link to a regular youtube link.')
+		.addStringOption((option) =>
+			option
+				.setName('link')
+				.setDescription('paste the link to the youtube short')
+				.setRequired(true))
+		.addBooleanOption((option) =>
+			option
+				.setName('lengthy')
+				.setDescription('returns non-shortened link if set to true')
+				.setRequired(false)),
+	
+	run: ({ interaction }) => {
+		const u = interaction.member.displayName;
+		const link = interaction.options.get('link').value;
+		const long = interaction.options.get('lengthy')?.value;
+		const IDstart = link.indexOf('shorts/');
+		const ID = link.slice(IDstart + 7);
+
+		try {
+			if (long == true) {
+				interaction.reply({
+					content: `https://www.youtube.com/watch?v=${ID}`,
+					ephemeral: true
+				});
+				console.log(`UTIL: ${u} converted a YT short link (lengthened)`);
+			} else {
+				interaction.reply({
+					content: `https://youtu.be/${ID}`,
+					ephemeral: true
+				});
+				console.log(`UTIL: ${u} converted a YT short link`);
+			};
+
+		} catch (error) {
+			console.log(`Error converting to normal YT link: ${error}`);
+		}
+	},
+}
