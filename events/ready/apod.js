ba6c31bc2c271e51206e815f03fc9c3ec53687fd
Brandt Hughes <brandt@brandthughes.com>
2023-06-21 20:38:47 -0500

    cron and console logs
    Cron should now work, posting APOD every morning at 10AM. setInterval has been replaced. Console logs also output a category of log for easy skimming.

--- events/ready/apod.js
+++ events/ready/apod.js
@@ -1,36 +1,58 @@
 require('dotenv').config();
 const { EmbedBuilder } = require('discord.js');
 const { request } = require('undici');
+var cron = require('node-cron');
 
 module.exports = async (client) => {
+
 	let testChannel = client.channels.cache.find(channel => channel.id === process.env.APOD_CHANNEL);
-	let day = 1000 * 60 * 60 * 24;
+	
+	console.log('CRON ___ scheduling APOD to post every day at 10:00 US Central.');
 
-	setInterval(async () => {
+	try {
+		cron.schedule('0 10 * * *', async () => {
+			let apodResponse = await request(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API}`);
+			let data = await apodResponse.body.json();
 
-		let apodResponse = await request(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API}`);
-		let data = await apodResponse.body.json();
+			let permaLink = `https://apod.nasa.gov/apod/ap${data.date.slice(2, 4) + data.date.slice(5, 7) + data.date.slice(8)}.html`;
 
-		let permaLink = `https://apod.nasa.gov/apod/ap${data.date.slice(2,4)+data.date.slice(5,7)+data.date.slice(8)}.html`;
+			// code adapted from Raxlitude's Discord Daily NASA APOD Posts on Autocode
+			// checks if `copyright` field is present in the JSON data - if, then includes `footer` field of embed to display copyright holder(s)
+			if ('copyright' in data) {
+				// checks if the post type is image & copyrighted
+				if (data.media_type === 'image') {
+					let embed = new EmbedBuilder()
+						.setTitle(data.title)
+						.setURL(permaLink)
+						.setDescription(data.explanation)
+						.setImage(`${data.url}`)
+						.setColor(0x0165b3)
+						.setFooter({ text: `Copyright ${data.copyright}` }); // please keep this due to NASA's restrictions
+					
+					await testChannel.send({ embeds: [embed] });
+					console.log('APOD ___ embed sent with image and copyright');
 
-		// code adapted from Raxlitude's Discord Daily NASA APOD Posts on Autocode
-		// checks if `copyright` field is present in the JSON data - if, then includes `footer` field of embed to display copyright holder(s)
-		if ('copyright' in data) {
-			// checks if the post type is image & copyrighted
-			if (data.media_type === 'image') {
-				let embed = new EmbedBuilder()
-					.setTitle(data.title)
-					.setURL(permaLink)
-					.setDescription(data.explanation)
-					.setImage(`${data.url}`)
-					.setColor(0x0165b3)
-					.setFooter({ text: `Copyright ${data.copyright}` }); // please keep this due to NASA's restrictions
-				
-				await testChannel.send({ embeds: [embed] });
-				console.log('embed sent with image and copyright');
+					// sending a different message if the post is not an image & copyrighted
+				} else {
+					let embed = new EmbedBuilder()
+						.setTitle(data.title)
+						.setURL(permaLink)
+						.setDescription(data.explanation)
+						.setImage(`${data.thumbnail}`)
+						.setColor(0x0165b3)
+						.addFields({
+							name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
+							value: data.url,
+							inline: false,
+						})
+						.setFooter({ text: `Copyright ${data.copyright}` }); // please keep this due to NASA's restrictions
+										
+					await testChannel.send({ embeds: [embed] });
+					console.log('APOD ___ embed sent w/o image but with copyright');
+				}
 
-			// sending a different message if the post is not an image & copyrighted
-			} else {
+				// checks if the post type is a video and not copyrighted
+			} else if (data.media_type === 'video') {
 				let embed = new EmbedBuilder()
 					.setTitle(data.title)
 					.setURL(permaLink)
@@ -41,42 +63,25 @@ module.exports = async (client) => {
 						name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
 						value: data.url,
 						inline: false,
-					})
-					.setFooter({ text: `Copyright ${data.copyright}` }); // please keep this due to NASA's restrictions
-									
-				await testChannel.send({ embeds: [embed] });
-				console.log('embed sent w/o image but with copyright');
-			}
-
-		// checks if the post type is a video and not copyrighted
-		} else if (data.media_type === 'video') {
-			let embed = new EmbedBuilder()
-				.setTitle(data.title)
-				.setURL(permaLink)
-				.setDescription(data.explanation)
-				.setImage(`${data.thumbnail}`)
-				.setColor(0x0165b3)
-				.addFields({
-					name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
-					value: data.url,
-					inline: false,
-				});
-		
-			await testChannel.send({ embeds: [embed] });
-			console.log('embed sent with video placeholder but no copyright');
+					});
 			
-		// checks if the post type is an image and not copyrighted
-		} else if (data.media_type === 'image') {
-			let embed = new EmbedBuilder()
-				.setTitle(data.title)
-				.setURL(permaLink)
-				.setDescription(data.explanation)
-				.setImage(`${data.url}`)
-				.setColor(0x0165b3);
-		
-			await testChannel.send({ embeds: [embed] });
-			console.log('embed sent with image but no copyright');
-		}
-	}, day
-	);
+				await testChannel.send({ embeds: [embed] });
+				console.log('APOD ___ embed sent with video placeholder but no copyright');
+				
+				// checks if the post type is an image and not copyrighted
+			} else if (data.media_type === 'image') {
+				let embed = new EmbedBuilder()
+					.setTitle(data.title)
+					.setURL(permaLink)
+					.setDescription(data.explanation)
+					.setImage(`${data.url}`)
+					.setColor(0x0165b3);
+			
+				await testChannel.send({ embeds: [embed] });
+				console.log('APOD ___ embed sent with image but no copyright');
+			}
+		});
+	} catch (error) {
+		console.log(`ERROR ___ Cron failed unexpectedly: ${error}`);
+	};
 };
