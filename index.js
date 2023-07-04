cf6046154e30f95c98b4be5a7a96c235fff0706d
Brandt Hughes <brandt@brandthughes.com>
2023-07-04 18:49:22 -0500

    removed timestamp from logs

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ const client = new Client({
 });
 
 (async () => {
-	const timezoneOffset = -5;
+	/*const timezoneOffset = -5;
 	const dBase = new Date();
 	dBase.setHours(dBase.getHours() + timezoneOffset);
 	const d = dBase.toISOString();
-	const timestamp = `${d.slice(0, 10)} | ${d.slice(11, 19)} |`;
+	const timestamp = `${d.slice(0, 10)} | ${d.slice(11, 19)} |`;*/
 
 	try {
 		await mongoose.connect(process.env.MONGODB_URI);
-		console.log(`${timestamp} BOOT ___ Connected to DB.`);
+		console.log(`BOOT ___ Connected to DB.`);
 
 		client.login(process.env.TOKEN);
-		console.log(`${timestamp} BOOT ___ Logged in.`);
+		console.log(`BOOT ___ Logged in.`);
 
 	} catch (error) {
-		console.log(`${timestamp} ERROR ___ ${error}`);
+		console.log(`ERROR ___ ${error}`);
 	}
 })();
 
@@ -37,5 +37,5 @@ new CommandHandler({
 	client,
 	commandsPath: path.join(__dirname, 'commands'),
 	eventsPath: path.join(__dirname, 'events'),
-	testServer: '722360419771220080',
+	//testServer: '722360419771220080',
 });
