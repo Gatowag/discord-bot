ff800777f5b80c3bf9f0a25c090ff75c6abb6f2b
Brandt Hughes <brandt@brandthughes.com>
2023-07-04 18:47:24 -0500

    commented out timestamp
    hopefully a temporary measure, something's acting strangely

--- events/ready/boot-sequence.js
+++ events/ready/boot-sequence.js
@@ -4,13 +4,13 @@ module.exports = (client) => {
 
 	console.log(`DIAG ___ started boot-sequence.`);
 
-	const timezoneOffset = -5;
+	/*const timezoneOffset = -5;
 	const dBase = new Date();
 	dBase.setHours(dBase.getHours() + timezoneOffset);
 	const d = dBase.toISOString();
-	const timestamp = `${d.slice(0, 10)} | ${d.slice(11, 19)} |`;
+	const timestamp = `${d.slice(0, 10)} | ${d.slice(11, 19)} |`;*/
 
-	console.log(`${timestamp} BOOT ___ ${client.user.tag} is online.`);
+	console.log(`BOOT ___ ${client.user.tag} is online.`);
 
 	client.user.setActivity({
 		name: 'Emergent Beacon',
