3158b8a37ae35ad8c1e325ffd2b79249e6ec2925
Brandt Hughes <brandt@brandthughes.com>
2023-07-17 08:04:25 -0500

    added random parameter
    This apod function can now pull a random image by setting Random to true. The embed timestamp reflects the date of the post (however the time is a result of the random generation and to the best of my knowledge cannot be sliced off).

--- commands/apod2.js
+++ commands/apod2.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { request } = require('undici');
 const timestamp = require('../utils/timestamp');
+const ms = require('ms');
 require('dotenv').config();
 
 module.exports = {
@@ -11,21 +12,44 @@ module.exports = {
 		.addStringOption((option) =>
 			option
 				.setName('date')
-				.setDescription('YYYY-MM-DD of the desired APOD')),
+				.setDescription('YYYY-MM-DD of the desired APOD (must be after 1995-06-16)'))
+		.addBooleanOption((option) =>
+			option
+				.setName('random')
+				.setDescription('will choose an APOD at random')),
 	
 	run: async ({ interaction }) => {
 
 		const dateIn = interaction.options.get('date')?.value;
-		(dateIn == null) ? (dateOut = "") : (dateOut = `&date=${dateIn}`);
-
+		const rand = interaction.options.get('random')?.value;
+		
 		const u = interaction.member.displayName;
-		const chan = interaction.channel;
 
 		try {
+			// notifies user "<application> is thinking..." and prevents error message that application did not respond
+			let quiet = await interaction.deferReply();
+
+			(!dateIn) ? (dateOut = "") : (dateOut = `&date=${dateIn}`);
+
+			let msDiff;
+			if (rand) {
+				const dEnd = new Date();
+				const dStart = new Date("1995-06-16");
+				let dRand = new Date(+dStart + Math.random() * (dEnd - dStart));
+				let dISO = dRand.toISOString();
+				msDiff = Math.abs(dEnd - dRand) * -1;
+				console.log(`dISO: ${dISO}`);
+				dateOut = `&date=${dISO.slice(0, 10)}`;
+			};
+
+			console.log(`msDiff: ${msDiff}`);
+			
 			let apodResponse = await request(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API}${dateOut}`);
 			let data = await apodResponse.body.json();
 
 			let permaLink = `https://apod.nasa.gov/apod/ap${data.date.slice(2, 4) + data.date.slice(5, 7) + data.date.slice(8)}.html`;
+
+			console.log(data.date);
 
 			// code adapted from Raxlitude's Discord Daily NASA APOD Posts on Autocode
 			// checks if `copyright` field is present in the JSON data - if, then includes `footer` field of embed to display copyright holder(s)
@@ -39,9 +63,10 @@ module.exports = {
 						.setDescription(data.explanation)
 						.setImage(`${data.url}`)
 						.setColor(0x0165b3)
+						.setTimestamp(Date.now() + msDiff)
 						.setFooter({ text: `©️ ${data.copyright.trim().replaceAll(' ,',',')}` }); // please keep this due to NASA's restrictions
 					
-					await chan.send({ embeds: [embed] });
+					await quiet.edit({ embeds: [embed] });
 					console.log(`${timestamp()} APOD ___ ${u} sent embed with image and copyright`);
 
 				// sending a different message if the post is not an image & copyrighted
@@ -57,9 +82,10 @@ module.exports = {
 							value: data.url,
 							inline: false,
 						})
+						.setTimestamp(Date.now() + msDiff)
 						.setFooter({ text: `©️ ${data.copyright.trim().replaceAll(' ,',',')}` }); // please keep this due to NASA's restrictions
 										
-					await chan.send({ embeds: [embed] });
+					await quiet.edit({ embeds: [embed] });
 					console.log(`${timestamp()} APOD ___ ${u} sent embed w/o image but with copyright`);
 				}
 
@@ -71,13 +97,14 @@ module.exports = {
 					.setDescription(data.explanation)
 					.setImage(`${data.thumbnail}`)
 					.setColor(0x0165b3)
+					.setTimestamp(Date.now() + msDiff)
 					.addFields({
 						name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
 						value: data.url,
 						inline: false,
 					});
 			
-				await chan.send({ embeds: [embed] });
+				await quiet.edit({ embeds: [embed] });
 				console.log(`${timestamp()} APOD ___ ${u} sent embed with video placeholder but no copyright`);
 				
 			// checks if the post type is an image and not copyrighted
@@ -87,11 +114,13 @@ module.exports = {
 					.setURL(permaLink)
 					.setDescription(data.explanation)
 					.setImage(`${data.url}`)
-					.setColor(0x0165b3);
+					.setColor(0x0165b3)
+					.setTimestamp(Date.now() + msDiff);
 			
-				await chan.send({ embeds: [embed] });
+				await quiet.edit({ embeds: [embed] });
 				console.log(`${timestamp()} APOD ___ ${u} sent embed with image but no copyright`);
 			}
+
 		} catch (error) {
 			console.log(`${timestamp()} ERROR ___ ${u}'s APOD request failed unexpectedly: ${error}`);
 		};
