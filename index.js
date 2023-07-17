cdb2821e758dfa4a274d00c8df5eb8eac3132d12
Brandt Hughes <brandt@brandthughes.com>
2023-07-17 05:03:41 -0500

    timestamp redirect
    timestamp is now a function called from the utils folder; cleans up the code a bit and allows for timestamp consistency across all command logs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
 const { CommandHandler } = require('djs-commander');
+const timestamp = require('utils/timestamp');
 const mongoose = require('mongoose');
 const path = require('path');
 
@@ -15,21 +16,15 @@ const client = new Client({
 });
 
 (async () => {
-	const timezoneOffset = -5;
-	const dBase = new Date();
-	dBase.setHours(dBase.getHours() + timezoneOffset);
-	const d = dBase.toISOString();
-	const timestamp = `${d.slice(0, 10)} | ${d.slice(11, 19)} |`;
-
 	try {
 		await mongoose.connect(process.env.MONGODB_URI);
-		console.log(`${timestamp} BOOT ___ Successfully connected to DB.`);
+		console.log(`${timestamp()} BOOT ___ Successfully connected to database.`);
 
 		client.login(process.env.TOKEN);
-		console.log(`${timestamp} BOOT ___ Successfully logged in to Discord.`);
+		console.log(`${timestamp()} BOOT ___ Successfully logged in to Discord.`);
 
 	} catch (error) {
-		console.log(`${timestamp} ERROR ___ ${error}`);
+		console.log(`${timestamp()} ERROR ___ Couldn't log in properly: ${error}`);
 	}
 })();
 
