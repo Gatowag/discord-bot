1c79fc3c6c702ca7fc51182b31e8987308a9b5b7
Brandt Hughes <brandt@brandthughes.com>
2023-07-17 02:16:31 -0500

    const to let
    I thought some of my timestamps on scheduled posts were acting strange, and I suspect it might be because the timestamp variables weren't allowed to update. idk

--- utils/timestamp.js
+++ utils/timestamp.js
@@ -1,9 +1,9 @@
 function timestamp() {
 	const timezoneOffset = -5;
-	const dBase = new Date();
+	let dBase = new Date();
 	dBase.setHours(dBase.getHours() + timezoneOffset);
-	const d = dBase.toISOString();
-	const timestamp = `${d.slice(0, 10)} | ${d.slice(11, 19)} |`;
+	let d = dBase.toISOString();
+	let timestamp = `${d.slice(0, 10)} | ${d.slice(11, 19)} |`;
 
 	return timestamp;
 }
