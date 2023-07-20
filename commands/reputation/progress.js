c6ae11f7d3e90ead9ce3449115d538f522b7b762
Brandt Hughes <brandt@brandthughes.com>
2023-07-20 00:46:19 -0500

    bar function and restructure
    Restructured some code to fall into the if statement so it isn't run unless needed. The creation of the progress bar has also been outsourced to its own function to make the code more readable.

--- commands/reputation/progress.js
+++ commands/reputation/progress.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const Level = require('../../models/levelStructure');
 const levelScaling = require('../../utils/levelScaling');
 const timestamp = require('../../utils/timestamp');
+const progressBar = require('../../utils/repProgress');
 
 module.exports = {
 	deleted: false,
@@ -10,7 +11,7 @@ module.exports = {
 		.setDescription('Check on your own reputation stats.'),
 	
 	run: async ({ interaction }) => {
-		
+
 		const u = interaction.user;
 		const uName = interaction.member.displayName;
 
@@ -19,46 +20,39 @@ module.exports = {
 			guildId: interaction.guild.id,
 		};
 
-		const channelUnlocks = [
-			6,
-			9,
-			4,
-			3,
-			1
-		];
+		const channelUnlocks = [ 6, 9, 4, 3, 1 ];
 
 		try {
 			const level = await Level.findOne(query);
-			const perc = Math.round((level.rep / levelScaling(level.level))*100)/100;
-			const progressFill = '▨';
-			const progressEmpty = '▢';
-			const progressBar = `${progressFill.repeat(Math.round(perc * 20))}${progressEmpty.repeat(20 - Math.round(perc * 20))}`;
-			let i = level.level;
-			let unlocksRemain = 0;
-			while (i++ < channelUnlocks.length - 1) {
-				unlocksRemain += channelUnlocks[i];
-			};
-
-			let unlockMsg;
-			(unlocksRemain == 0) ? unlockMsg = '' : unlockMsg = `\nYou have ${unlocksRemain} channel${unlocksRemain == 1 ? '' : 's'} left to unlock.`;
 			
-			const embed = new EmbedBuilder()
-			.setTitle('your reputation stats')
-			.setDescription(`You are ${Math.floor(perc * 100)}% to level ${level.level + 1}.\n${progressBar}${unlockMsg}`)
-				.setColor('Gold')
-				.setThumbnail(u.displayAvatarURL());
-
-
 			if (level) {
+				const stats = {
+					lvl: level.level,
+					threshold: levelScaling(level.level),
+					rep: level.rep,
+					percent: Math.round((level.rep / levelScaling(level.level)) * 100) / 100 };
+				let i = stats.lvl;
+				let unlocksRemain = 0;
+				while (i++ < channelUnlocks.length - 1) {
+					unlocksRemain += channelUnlocks[i];
+				};
+
+				let unlockMsg;
+				(unlocksRemain == 0) ? unlockMsg = '' : unlockMsg = `\nYou have ${unlocksRemain} channel${unlocksRemain == 1 ? '' : 's'} left to unlock.`;
+				
+				const embed = new EmbedBuilder()
+				.setTitle('your reputation stats')
+				.setDescription(`\`\`\` ${progressBar(stats.threshold, stats.rep)} \n ${Math.floor(stats.percent * 100)}% to level ${stats.lvl + 1}\`\`\`${unlockMsg}`)
+					.setColor('Gold')
+					.setThumbnail(u.displayAvatarURL());
+
 				interaction.reply({
 					embeds: [embed],
-					//content: `### your progression stats\nYou are ${Math.floor(perc*100)}% to level ${level.level+1}.\n${progressBar}${unlockMsg}`,
 					ephemeral: true
 				});
 
 				console.log(`${timestamp()} REP CHECK ___ ${uName} requested a progression check on themselves.`);
-			}
-			else {
+			} else {
 				console.log(`${timestamp()} DATABASE ___ creating new entry for ${uName}`);
 				//create new level
 				const newLevel = new Level({
