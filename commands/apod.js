0fffad965aee85102cf2205b9a3b35f03ba20c77
Brandt Hughes <brandt@brandthughes.com>
2023-07-20 00:35:27 -0500

    call apod embed function
    simplify code by calling command

--- commands/apod.js
+++ commands/apod.js
@@ -3,6 +3,7 @@ const { request } = require('undici');
 const timestamp = require('../utils/timestamp');
 const ms = require('ms');
 require('dotenv').config();
+const apodEmbed = require('../utils/apodEmbed');
 
 module.exports = {
 	deleted: false,
@@ -29,88 +30,10 @@ module.exports = {
 			// notifies user "<application> is thinking..." and prevents error message that application did not respond
 			let quiet = await interaction.deferReply();
 
-			(!dateIn) ? (dateOut = "") : (dateOut = `&date=${dateIn}`);
-
-			if (rand) {
-				const dEnd = new Date();
-				const dStart = new Date("1995-06-16");
-				let dRand = new Date(+dStart + Math.random() * (dEnd - dStart));
-				let dISO = dRand.toISOString();
-				dateOut = `&date=${dISO.slice(0, 10)}`;
-			};
+			let embed = await apodEmbed(dateIn, rand);
 			
-			let apodResponse = await request(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API}${dateOut}`);
-			let data = await apodResponse.body.json();
-
-			let permaLink = `https://apod.nasa.gov/apod/ap${data.date.slice(2, 4) + data.date.slice(5, 7) + data.date.slice(8)}.html`;
-
-			// code adapted from Raxlitude's Discord Daily NASA APOD Posts on Autocode
-			// checks if `copyright` field is present in the JSON data - if, then includes `footer` field of embed to display copyright holder(s)
-			if ('copyright' in data) {
-
-				// checks if the post type is image & copyrighted
-				if (data.media_type === 'image') {
-					let embed = new EmbedBuilder()
-						.setTitle(data.title)
-						.setURL(permaLink)
-						.setDescription(data.explanation)
-						.setImage(`${data.url}`)
-						.setColor(0x0165b3)
-						.setFooter({ text: `©️ ${data.copyright.trim().replaceAll(' ,',',')}\n${data.date}` }); // please keep this due to NASA's restrictions
-					
-					await quiet.edit({ embeds: [embed] });
-					console.log(`${timestamp()} APOD ___ ${u} sent embed with image and copyright`);
-
-				// sending a different message if the post is not an image & copyrighted
-				} else {
-					let embed = new EmbedBuilder()
-						.setTitle(data.title)
-						.setURL(permaLink)
-						.setDescription(data.explanation)
-						.setImage(`${data.thumbnail}`)
-						.setColor(0x0165b3)
-						.addFields({
-							name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
-							value: data.url,
-							inline: false,
-						})
-						.setFooter({ text: `©️ ${data.copyright.trim().replaceAll(' ,',',')}\n${data.date}` }); // please keep this due to NASA's restrictions
-										
-					await quiet.edit({ embeds: [embed] });
-					console.log(`${timestamp()} APOD ___ ${u} sent embed w/o image but with copyright`);
-				}
-
-			// checks if the post type is a video and not copyrighted
-			} else if (data.media_type === 'video') {
-				let embed = new EmbedBuilder()
-					.setTitle(data.title)
-					.setURL(permaLink)
-					.setDescription(data.explanation)
-					.setImage(`${data.thumbnail}`)
-					.setColor(0x0165b3)
-					.addFields({
-						name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
-						value: data.url,
-						inline: false,
-					})
-					.setFooter({ text: `${data.date}` });
-			
-				await quiet.edit({ embeds: [embed] });
-				console.log(`${timestamp()} APOD ___ ${u} sent embed with video placeholder but no copyright`);
-				
-			// checks if the post type is an image and not copyrighted
-			} else if (data.media_type === 'image') {
-				let embed = new EmbedBuilder()
-					.setTitle(data.title)
-					.setURL(permaLink)
-					.setDescription(data.explanation)
-					.setImage(`${data.url}`)
-					.setColor(0x0165b3)
-					.setFooter({ text: `${data.date}` });
-			
-				await quiet.edit({ embeds: [embed] });
-				console.log(`${timestamp()} APOD ___ ${u} sent embed with image but no copyright`);
-			}
+			await quiet.edit({ embeds: [embed] });
+			console.log(`${timestamp()} APOD  ▨  ${u} successfully sent apod post`);
 
 		} catch (error) {
 			console.log(`${timestamp()} ERROR ___ ${u}'s APOD request failed unexpectedly: ${error}`);
--- events/ready/apod-scheduled.js
+++ events/ready/apod-scheduled.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { EmbedBuilder } = require('discord.js');
 const { request } = require('undici');
 const timestamp = require('../../utils/timestamp');
+const apodEmbed = require('../../utils/apodEmbed');
 var cron = require('node-cron');
 
 module.exports = async (client) => {
@@ -11,78 +12,13 @@ module.exports = async (client) => {
 	console.log(`${timestamp()} BOOT  |  Scheduled APOD to post every day at 10:00 US Central.`);
 
 	try {
-		cron.schedule('0 15 * * *', async () => {
-			let apodResponse = await request(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API}`);
-			let data = await apodResponse.body.json();
-
-			let permaLink = `https://apod.nasa.gov/apod/ap${data.date.slice(2, 4) + data.date.slice(5, 7) + data.date.slice(8)}.html`;
-
-			// code adapted from Raxlitude's Discord Daily NASA APOD Posts on Autocode
-			// checks if `copyright` field is present in the JSON data - if, then includes `footer` field of embed to display copyright holder(s)
-			if ('copyright' in data) {
-				// checks if the post type is image & copyrighted
-				if (data.media_type === 'image') {
-					let embed = new EmbedBuilder()
-						.setTitle(data.title)
-						.setURL(permaLink)
-						.setDescription(data.explanation)
-						.setImage(`${data.url}`)
-						.setColor(0x0165b3)
-						.setFooter({ text: `©️ ${data.copyright.trim().replaceAll(' ,',',')}\n${data.date}` }); // please keep this due to NASA's restrictions
-					
-					await testChannel.send({ embeds: [embed] });
-					console.log(`${timestamp()} APOD ___ embed sent with image and copyright`);
-
-				// sending a different message if the post is not an image & copyrighted
-				} else {
-					let embed = new EmbedBuilder()
-						.setTitle(data.title)
-						.setURL(permaLink)
-						.setDescription(data.explanation)
-						.setImage(`${data.thumbnail}`)
-						.setColor(0x0165b3)
-						.addFields({
-							name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
-							value: data.url,
-							inline: false,
-						})
-						.setFooter({ text: `©️ ${data.copyright.trim().replaceAll(' ,',',')}\n${data.date}` }); // please keep this due to NASA's restrictions
-										
-					await testChannel.send({ embeds: [embed] });
-					console.log(`${timestamp()} APOD ___ embed sent w/o image but with copyright`);
-				}
-
-			// checks if the post type is a video and not copyrighted
-			} else if (data.media_type === 'video') {
-				let embed = new EmbedBuilder()
-					.setTitle(data.title)
-					.setURL(permaLink)
-					.setDescription(data.explanation)
-					.setImage(`${data.thumbnail}`)
-					.setColor(0x0165b3)
-					.addFields({
-						name: 'Discord doesn\'t allow videos in rich embeds, so click through to check it out.',
-						value: data.url,
-						inline: false,
-					})
-					.setFooter({ text: `${data.date}` });
+		cron.schedule('* 10 * * *', async () => {
+			let embed = await apodEmbed();
 			
-				await testChannel.send({ embeds: [embed] });
-				console.log(`${timestamp()} APOD ___ embed sent with video placeholder but no copyright`);
-				
-			// checks if the post type is an image and not copyrighted
-			} else if (data.media_type === 'image') {
-				let embed = new EmbedBuilder()
-					.setTitle(data.title)
-					.setURL(permaLink)
-					.setDescription(data.explanation)
-					.setImage(`${data.url}`)
-					.setColor(0x0165b3)
-					.setFooter({ text: `${data.date}` });
+			await apodChannel.send({ embeds: [embed] });
+			console.log(`${timestamp()} APOD  ▨  successfully posted daily morning post`);
+		});
 			
-				await testChannel.send({ embeds: [embed] });
-				console.log(`${timestamp()} APOD ___ embed sent with image but no copyright`);
-			}
 		});
 	} catch (error) {
 		console.log(`${timestamp()} ERROR ___ Cron failed unexpectedly: ${error}`);
