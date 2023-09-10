50d058d00f6dfff6b94b6c552ff5000808fb9709
Brandt Hughes <brandt@brandthughes.com>
2023-09-10 16:27:11 -0500

    rearranging code
    moved everything into the try block because those lines of code were lonely

--- events/ready/apod-scheduled.js
+++ events/ready/apod-scheduled.js
@@ -4,13 +4,9 @@ const apodEmbed = require('../../utils/apodEmbed');
 var cron = require('node-cron');
 
 module.exports = async (client) => {
-
-	let apodChannel = client.channels.cache.find(channel => channel.id === process.env.APOD_CHANNEL);
-	
-	console.log(`${timestamp()} BOOT  |  Scheduled APOD to post every day at 10:00 US Central.`);
-	console.log(`${timestamp()} BOOT  |  Scheduled APOD to post every day at 22:00 US Central.`);
-
 	try {
+		let apodChannel = client.channels.cache.find(channel => channel.id === process.env.APOD_CHANNEL);
+
 		cron.schedule('0 15 * * *', async () => {
 			let embed = await apodEmbed();
 			
@@ -24,6 +20,10 @@ module.exports = async (client) => {
 			await apodChannel.send({ embeds: [embed] });
 			console.log(`${timestamp()} APOD  ▨  successfully posted nightly random post`);
 		});
+
+		console.log(`${timestamp()} BOOT  |  Scheduled APOD to post every day at 10:00 US Central.`);
+		console.log(`${timestamp()} BOOT  |  Scheduled APOD to post every day at 22:00 US Central.`);
+
 	} catch (error) {
 		console.log(`${timestamp()} ERROR >!< cron failed unexpectedly: ${error}`);
 	};
