ac0303bb25448564a8ba59dfcf7cf48ba13dd765
Brandt Hughes <brandt@brandthughes.com>
2024-01-16 00:12:36 -0600

    minor change to poll results
    fixes the result percentages to 2 decimal places for situations where javascript does some funny business and doesn't cleanly round as expected

--- utils/pollProgress.js
+++ utils/pollProgress.js
@@ -3,19 +3,20 @@ function pollProgress(index, voteTotals) {
 	const fill = '▨', empty = '▢', size = 20;
 	// adds up all of our votes in this poll
 	const voteSum = voteTotals.reduce((a, b) => a + b, 0);
-	let percent;
+	let percent, percentDisplay;
 
 	// calculate percentage of votes, default to 0
 	if (voteTotals[index] == 0) {
 		percent = 0;
 	} else {
 		percent = Math.round(voteTotals[index] / voteSum * 100) / 100;
+		percentDisplay = percent.toFixed(2);
 	}
 
 	// build the progress bar using our comp/params and calculations
 	const progressBar = `${fill.repeat(Math.round(percent * size))}${empty.repeat(size - Math.round(percent * size))}`;
 	// place the progress bar inside a code block for more cross-platform consistency and add visible stat calculations
-	const newField = `\`\`\`${progressBar}\n ${voteTotals[index]} / ${voteSum}  ->  ${percent*100}% \`\`\``;
+	const newField = `\`\`\`${progressBar}\n ${voteTotals[index]} / ${voteSum}  ->  ${percentDisplay}% \`\`\``;
 
 	return newField;
 }
