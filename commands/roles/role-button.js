4dda2d906b8ed6417b3d6a0df9fac10a56b98e3f
Brandt Hughes <brandt@brandthughes.com>
2024-02-01 22:58:03 -0600

    comment text
    didn't remember anything so I provided comment text to better describe what's going on

--- commands/roles/role-button.js
+++ commands/roles/role-button.js
@@ -24,28 +24,33 @@ module.exports = {
 	
 	run: async ({ interaction }) => {
 		
+		// retrieve user input
 		const role1 = interaction.options.getRole('role');
 		const label1 = interaction.options.get('label')?.value;
 		const successOn = interaction.options.get('success-reply')?.value;
 		const successOff = `You have removed the role ${role1.name}`;
 
+		// create button
 		const button = new ButtonBuilder()
 			.setCustomId('button1')
 			.setLabel(`${label1}`)
 			.setStyle(ButtonStyle.Success);
 		
+		// if not provided, updates text on the button to this
 		if (!label1) {
 			button.setLabel(`${role1.name}`);
 		};
 		
+		// if not provided, default success text to this
 		if (!successOn) {
 			successOn = `You have added the role ${role1.name}`;
 		};
 		
+		// create button row in post
 		const buttonRow = new ActionRowBuilder()
 			.addComponents(
 				button,);
-		
+
 		await interaction.reply({ components: [buttonRow] });
 
 		console.log(`${timestamp()} BUTTON ___ ${interaction.member.displayName} created a button to toggle the ${role1.name} role.`)
@@ -53,14 +58,23 @@ module.exports = {
 		const collector = await interaction.channel.createMessageComponentCollector();
 
 		collector.on('collect', async (i) => {
+			// person who clicked on button
 			const member = i.member;
 
+			// if the interaction is clicking on the button
 			if (i.customId === 'button1') {
+
+				// if the person already has the role
 				if (member.roles.cache.has(role1)) {
+					// then remove that role, toggle off
 					member.roles.remove(role1);
+					// update the reply to quietly notify the person that they have removed the role
 					i.reply({ content: `${successOff}`, ephemeral: true });
+				// if the person doesn't have the role yet
 				} else {
+					// then add that role, toggle on
 					member.roles.add(role1);
+					// update the reply to quietly notify the person that they have added the role
 					i.reply({ content: `${successOn}`, ephemeral: true });
 				}
 				
