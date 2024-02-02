aac647bd7735b6bfbd72839a2447629e856facae
Brandt Hughes <brandt@brandthughes.com>
2024-02-02 01:40:03 -0600

    fixed toggling, button IDs, try/catch
    Previously all buttons created with this were given button1 as an ID and I think that broke interactions. Now they're given an ID specific to the role given to them, which I think works fine?
    
    Also the previous code didn't properly recognize whether or not the interacting user had the role, so toggling off didn't work. It should now.
    
    Now it's all in a try catch block, because that seems prudent. Sometimes when this command broke, it would force the bot to restart. Shouldn't have to now, I think.

--- commands/roles/role-button.js
+++ commands/roles/role-button.js
@@ -26,76 +26,83 @@ module.exports = {
 				.setRequired(false)),
 	
 	run: async ({ interaction }) => {
-		
-		// retrieve user input
-		const role1 = interaction.options.getRole('role');
-		const label1 = interaction.options.get('label')?.value;
-		const successOn = interaction.options.get('success-reply')?.value;
-		const successOff = `You have removed the role ${role1.name}`;
+		try {
+			// declares user input
+			const role = interaction.options.getRole('role');
+			let label = interaction.options.get('label')?.value;
+			let successOn = interaction.options.get('success-reply')?.value;
+			const successOff = `You have removed the role ${role.name}`;
+
 			diag && console.log(`${timestamp()} DIAG  ▢  creating role button`);
 			diag && console.log(`${timestamp()} DIAG  |  label1 input: ${label}`);
 			diag && console.log(`${timestamp()} DIAG  |  successOn input: ${successOn}`);
 
-		// create button
-		const button = new ButtonBuilder()
-			.setCustomId('button1')
-			.setLabel(`${label1}`)
-			.setStyle(ButtonStyle.Success);
+			// create button
+			const button = new ButtonBuilder()
+				.setCustomId(`button${role}`)
+				.setLabel(`${label}`)
+				.setStyle(ButtonStyle.Success);
 		
-		// if not provided, updates text on the button to this
-		if (!label1) {
-			button.setLabel(`${role1.name}`);
-		};
+			// if not provided, updates text on the button to this
+			if (!label) {
 				diag && console.log(`${timestamp()} DIAG  |  no label entered`);
+				button.setLabel(`${role.name}`);
 				diag && console.log(`${timestamp()} DIAG  |  set default label`);
+			};
 		
-		// if not provided, default success text to this
-		if (!successOn) {
-			successOn = `You have added the role ${role1.name}`;
-		};
+			// if not provided, default success text to this
+			if (!successOn) {
 				diag && console.log(`${timestamp()} DIAG  |  no reply text entered`);
+				successOn = `You have added the role ${role.name}`;
 				diag && console.log(`${timestamp()} DIAG  |  set default reply`);
+			};
 		
-		// create button row in post
-		const buttonRow = new ActionRowBuilder()
-			.addComponents(
-				button,);
+			// create button row in post
+			const buttonRow = new ActionRowBuilder()
+				.addComponents(
+					button,);
 
-		await interaction.reply({ components: [buttonRow] });
+			await interaction.reply({ components: [buttonRow] });
 
-		console.log(`${timestamp()} BUTTON ___ ${interaction.member.displayName} created a button to toggle the ${role1.name} role.`)
+			console.log(`${timestamp()} BUTTON ___ ${interaction.member.displayName} created a button to toggle the ${role.name} role.`)
 
-		const collector = await interaction.channel.createMessageComponentCollector();
+			const collector = await interaction.channel.createMessageComponentCollector();
 
-		collector.on('collect', async (i) => {
-			// person who clicked on button
-			const member = i.member;
+			collector.on('collect', async (i) => {
+				// person who clicked on button
+				const member = i.member;
+				let hasRole = interaction.member.roles.cache.has(role.id);
 				diag && console.log(`${timestamp()} DIAG  |  ${member}, ${member.displayName} interacted. Do they have the role? ${hasRole}`);
 
-			// if the interaction is clicking on the button
-			if (i.customId === 'button1') {
+				// if the interaction is clicking on the button
+				if (i.customId === `button${role}`) {
 					diag && console.log(`${timestamp()} DIAG  |  i.customId is ${i.customId}`);
 
-				// if the person already has the role
-				if (member.roles.cache.has(role1)) {
-					// then remove that role, toggle off
-					member.roles.remove(role1);
-					// update the reply to quietly notify the person that they have removed the role
-					i.reply({ content: `${successOff}`, ephemeral: true });
-				// if the person doesn't have the role yet
-				} else {
-					// then add that role, toggle on
-					member.roles.add(role1);
-					// update the reply to quietly notify the person that they have added the role
-					i.reply({ content: `${successOn}`, ephemeral: true });
-				}
+					// if the person already has the role
+					if (hasRole) {
+						// then remove that role, toggle off
+						member.roles.remove(role);
 						diag && console.log(`${timestamp()} DIAG  |  ${role.name} removed`);
+						// update the reply to quietly notify the person that they have removed the role
+						i.reply({ content: `${successOff}`, ephemeral: true });
 						diag && console.log(`${timestamp()} DIAG  ▨  reply sent`);
+						console.log(`${timestamp()} BUTTON ___ ${member.displayName} successfully removed the ${role.name} role.`);
+						// if the person doesn't have the role yet
+					} else {
+						// then add that role, toggle on
+						member.roles.add(role);
 						diag && console.log(`${timestamp()} DIAG  |  ${role.name} added`);
+						// update the reply to quietly notify the person that they have added the role
+						i.reply({ content: `${successOn}`, ephemeral: true });
 						diag && console.log(`${timestamp()} DIAG  ▨  reply sent`);
+						console.log(`${timestamp()} BUTTON ___ ${member.displayName} successfully added the ${role.name} role.`);
+					}
 				
-				console.log(`${timestamp()} BUTTON ___ ${member.displayName} was successfully granted the ${role1.name} role.`);
-			};
-		})
+				
+				};
+			})
+		} catch (error) {
+			console.log(`${timestamp()} ERROR >!< role button failed: ${error}`);
+		}
 	}
 };
