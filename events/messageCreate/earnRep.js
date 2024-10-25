52fb85c3b8fc6e4dc8972ab16afeb39403dbc142
Brandt Hughes <brandt@brandthughes.com>
2024-10-24 21:56:05 -0500

    updated level up messages
    also fixed a problem where lvl 5 messages weren't properly sending

--- events/messageCreate/earnRep.js
+++ events/messageCreate/earnRep.js
@@ -63,30 +63,37 @@ module.exports = async (message, client) => {
 							console.log(`${timestamp()} ROLES ___ ${u} was granted ${newRole.name} and revoked ${oldRole.name}`);
 						})
 						.catch(error => console.log(`${timestamp()} ERROR >!< unable to add and remove roles: ${error}`));
-					
-				};
-				
-				const lvlMessages = [
-					//0: sentient being
-					'',
-					//1: local dignitary
-					`Thank you for adding to the conversation, ${message.member}. Your candor convinced the electorate to elect you as **${newRole.name.slice(9)}**. With it, your access has expanded to 9 more channels. Be sure to check ${unlocksChannel} to see a breakdown. Be well.\n\n*This message self-terminates in 30 seconds.*`,
-					//2: planetary diplomat
-					`You have made a name for yourself, ${message.member}. Your planet entrusts you to represent them as a **${newRole.name.slice(9)}**. This comes with 4 more channels to better connect with the world you now represent. Be sure to check ${unlocksChannel} to see a breakdown. Keep exploring, keep questioning.\n\n*This message self-terminates in 30 seconds.*`,
-					//3: galactic ambassador
-					`We're honored to be present for your swearing in, **${newRole.name.slice(9)}** ${message.member}. The weight of our local system rests squarely on your shoulders, so enjoy these complementary 3 channels. We're confident this will ease your burden. Be sure to check ${unlocksChannel} to see a breakdown. Project your voice confidently into the dark reaches of the universe.\n\n*This message self-terminates in 30 seconds.*`,
-					//4: interdimensional explorer
-					`${message.member} glows brightly amongst the salted sky as an **${newRole.name.slice(9)}**. Their vast experience inherits a trust required for the last remaining channel. Be sure to check ${unlocksChannel} to see a breakdown. Knowledge can never be quenched nor suppressed to extinction so long as someone exists to pursue it.\n\n*This message self-terminates in 30 seconds.*`,
-					//5+ ???
-					`${message.member} just reached level ${userData.level}. At this point you don't get new roles or channels, you just get a little older and a little better. Until next time...\n\n*This message self-terminates in 30 seconds.*`
-				];
-				
-				console.log(`${timestamp()} LVL UP ___ user: ${u}, from lvl ${oldLvl} to ${userData.level}`);
 
-				message.reply(lvlMessages[lvlMsgNum])
-					.then(msg => {
-						setTimeout(() => msg.delete(), 30000);
-					});
+					const lvlMessages = [
+						//0: sentient being
+						'',
+						//1: local dignitary
+						`Thank you for adding to the conversation, ${message.member}. Your candor convinced the electorate to elect you as **${newRole.name.slice(9)}**. With it, your access has expanded to 9 more channels. Be sure to check ${unlocksChannel} to see a breakdown. Be well.\n\n*This message self-terminates in 30 seconds.*`,
+						//2: planetary diplomat
+						`You have made a name for yourself, ${message.member}. Your planet entrusts you to represent them as a **${newRole.name.slice(9)}**. This comes with 4 more channels to better connect with the world you now represent. Be sure to check ${unlocksChannel} to see a breakdown. Keep exploring, keep questioning.\n\n*This message self-terminates in 30 seconds.*`,
+						//3: galactic ambassador
+						`We're honored to be present for your swearing in, **${newRole.name.slice(9)}** ${message.member}. The weight of our local system rests squarely on your shoulders, so enjoy these complementary 3 channels. We're confident this will ease your burden. Be sure to check ${unlocksChannel} to see a breakdown. Project your voice confidently into the dark reaches of the universe.\n\n*This message self-terminates in 30 seconds.*`,
+						//4: interdimensional explorer
+						`${message.member} glows brightly amongst the salted sky as an **${newRole.name.slice(9)}**. Their vast experience inherits a trust required for the last remaining channel. Be sure to check ${unlocksChannel} to see a breakdown. Knowledge can never be quenched nor suppressed to extinction so long as someone exists to pursue it.\n\n*This message self-terminates in 30 seconds.*`,
+					];
+				
+					console.log(`${timestamp()} LVL UP ___ user: ${u}, from lvl ${oldLvl} to ${userData.level}`);
+
+					message.reply(lvlMessages[lvlMsgNum])
+						.then(msg => {
+							setTimeout(() => msg.delete(), 30000);
+						});
+					
+				} else {
+					const lvlMessage = `${message.member} just reached level ${userData.level}. At this point you don't get new roles or channels, you just get a little older and a little better. Until next time...\n\n*This message self-terminates in 30 seconds.*`;
+
+					console.log(`${timestamp()} LVL UP ___ user: ${u}, from lvl ${oldLvl} to ${userData.level}`);
+
+					message.reply(lvlMessage)
+						.then(msg => {
+							setTimeout(() => msg.delete(), 30000);
+						});
+				}
 			}
 
 			// update the database data and discontinue process
